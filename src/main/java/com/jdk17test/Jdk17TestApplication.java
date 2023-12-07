3e53de18725c10d8868b203595a81269e2d883a1
ChangSeongChu <jacob@puzzlesystems.co.kr>
2023-12-07 10:49:57 +0900

    first commit

---
+++ src/main/java/com/jdk17test/Jdk17TestApplication.java
@@ -0,0 +1,13 @@
+package com.jdk17test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdk17TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jdk17TestApplication.class, args);
+    }
+
+}
